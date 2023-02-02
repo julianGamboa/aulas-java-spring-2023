6cfdab7923bcc5a314a9b661e77f0be8c5fc5291
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2023-02-02 04:26:42 -0300

    forma muito simples sem definir varios usuarios

--- aula-01-basica-seguranca/src/main/java/julian/example/julian/JulianApplication.java
+++ aula-01-basica-seguranca/src/main/java/julian/example/julian/JulianApplication.java
@@ -2,19 +2,21 @@ package julian.example.julian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@SpringBootApplication
-@RestController  //(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
+@RestController  
 public class JulianApplication {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public ModelAndView index () {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("formularioEntradaVeiculos");
+		
 		return modelAndView;
 	}
 
