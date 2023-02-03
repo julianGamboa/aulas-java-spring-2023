a3b0e4d711530556b0898ef1b9be185e52f4e95a
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2023-02-03 15:26:16 -0300

    Aprimorandoo gitignore

--- aula-02-seguranca-configuracoes/src/main/java/julian/example/julian/JulianApplication.java
+++ aula-02-seguranca-configuracoes/src/main/java/julian/example/julian/JulianApplication.java
@@ -3,22 +3,12 @@ package julian.example.julian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
 @RestController  
 public class JulianApplication {
-
-	@GetMapping("/for")
-	public ModelAndView index () {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("formularioEntradaVeiculos");
-		
-		return modelAndView;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JulianApplication.class, args);
